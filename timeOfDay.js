a848edc9b03f046f634baa6d6b0e8c9af2e33fda
grimer30 <mstout64@gmail.com>
2015-06-22 03:15:51 +0100

    Added moon and fixed sun/moon offset
    The offset isn't perfect, but it's acceptable and they only move when
    the page is refreshed so it shouldn't matter too much.

--- timeOfDay.js
+++ timeOfDay.js
@@ -1,18 +1,32 @@
 var now = new Date();
-var hours = now.getHours();
+var hours = now.getHours()+0;
 var time = hours + now.getMinutes()/60;
 var timePercent = time/24*100;
 
 
 $(document).ready(function() {
 
-  $("#sun").css({
-    'left': (timePercent/2) - 25 + '%',
-    'top': '5%',
-  });
-
-  
   $('#sky').addClass("sky-gradient-"+hours);
 
+  $("#sun").css({
+    'left': (timePercent*2) - 60 + '%',
+    'top': '5%',
+  });
   
+  if (timePercent<26){
+    $("#moon").css({
+      'left': (timePercent*2) + 60 + '%',
+      'top': '5%',
+    });
+  }
+  if (timePercent>76){
+    $("#moon").css({
+      'left': (timePercent*2) - 160 + '%',
+      'top': '5%',
+    });
+  }
+  
+
+
 });
+
