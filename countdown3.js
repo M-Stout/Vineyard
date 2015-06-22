feef6a1e06b72e852fe122ac1ae446406c4e9700
grimer30 <mstout64@gmail.com>
2015-06-22 18:23:21 +0100

    Added countdown for air justin's party, it's happening very soon!

---
+++ countdown3.js
@@ -0,0 +1,24 @@
+function updateWCTime() {
+	now      = new Date();
+	kickoff  = Date.parse("June 22, 2015 16:00:00 EST");
+	diff = kickoff - now;
+
+	days  = Math.floor( diff / (1000*60*60*24) );
+	hours = Math.floor( diff / (1000*60*60) );
+	mins  = Math.floor( diff / (1000*60) );
+	secs  = Math.floor( diff / 1000 );
+
+	dd = days;
+	hh = hours - days  * 24;
+	mm = mins  - hours * 60;
+	ss = secs  - mins  * 60;
+
+        document.getElementById("countdown3")
+            .innerHTML =
+              'Justin Card party in: <br>' +
+                dd + ' days ' +
+                hh + ' hours ' +
+                mm + ' minutes ' +
+                ss + ' seconds';
+}
+setInterval(updateWCTime, 1000 );
